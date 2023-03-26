ea6339fd3316f7ead4424822c7ce22a8d6b8be1b
sayyedsiddique <sayyedsiddique055@gmail.com>
2023-03-26 13:58:40 +0530

    old project removed

--- src/pages/about.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const about = () => {
-  return (
-    <div>Hello about how are you</div>
-  )
-}
-
-export default about
--- src/pages/blogpost/how-to-use-keyboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const how = () => {
-  return (
-    <div>how to use keyboard</div>
-  )
-}
-
-export default how
